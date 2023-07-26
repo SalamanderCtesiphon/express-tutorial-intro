6115989b64e62b555f5a7a7c5279411a0bf76a5e
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-26 14:25:23 -0500

    added the routes for site navigation

--- express-crash-course/server.js
+++ express-crash-course/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   console.log("here");
-  res.render('index')
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact-me');
+});
+
+app.get('/404', (req, res) => {
+  res.render('404');
+});
+
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
 
 app.listen(3000);
 
